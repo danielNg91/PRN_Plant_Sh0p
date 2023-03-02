e37a339cd6a35f5a1dc63d79ca7be3916d304862
lcht2701 <lecanhhoangthanh@gmail.com>
2023-03-02 23:24:10 +0700

    Add DropBox for categories in Product Create + Edit

--- LandingPage/Pages/Products/Create.cshtml.cs
+++ LandingPage/Pages/Products/Create.cshtml.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Persistence.Models;
@@ -9,15 +10,20 @@ namespace PlantShop.Pages.Products
     public class CreateModel : PageModel
     {
         private readonly GenericRepository<Product> _productRepository;
-        [BindProperty]
-        public Product Product { get; set; }
-        
-        public CreateModel(GenericRepository<Product> productRepository)
+        private readonly GenericRepository<ProductCategory> _categoryRepository;
+        [BindProperty] public Product Product { get; set; }
+        public IEnumerable<ProductCategory> Categories { get; set; }
+
+        public CreateModel(GenericRepository<Product> productRepository, GenericRepository<ProductCategory> categoryRepository)
         {
             _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
         }
-
-        public async  Task<IActionResult> OnPostAsync()
+        public async Task OnGetAsync()
+        {
+            Categories = await _categoryRepository.ListAsync();
+        }
+        public async Task<IActionResult> OnPostAsync()
         {
             if (ModelState.IsValid)
             {
@@ -25,6 +31,7 @@ namespace PlantShop.Pages.Products
                 TempData["success"] = "Product created successfully";
                 return RedirectToPage("Index");
             }
+
             return Page();
         }
     }
--- LandingPage/Pages/Products/Edit.cshtml.cs
+++ LandingPage/Pages/Products/Edit.cshtml.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Persistence.Models;
@@ -10,16 +11,21 @@ namespace PlantShop.Pages.Products
     public class EditModel : PageModel
     {
         private readonly GenericRepository<Product> _productRepository;
+        private readonly GenericRepository<ProductCategory> _categoryRepository;
         [BindProperty]
         public Product Product { get; set; }
+        public IEnumerable<ProductCategory> Categories { get; set; }
 
-        public EditModel(GenericRepository<Product> productRepository)
+        public EditModel(GenericRepository<Product> productRepository, GenericRepository<ProductCategory> categoryRepository)
         {
             _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
         }
+
         public async Task OnGetAsync(int id)
         {
             Product = await _productRepository.FindByIdAsync(id);
+            Categories = await _categoryRepository.ListAsync();
         }
 
         public async Task<IActionResult> OnPostAsync()
