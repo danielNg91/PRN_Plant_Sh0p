7f2b6af6f944d17dab38653331978800cf685289
hado9 <hado@intelisysaviation.com>
2023-03-05 11:53:01 +0700

    update UI for Home page

--- LandingPage/Pages/Index.cshtml.cs
+++ LandingPage/Pages/Index.cshtml.cs
@@ -5,21 +5,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Persistence.Models;
+using Persistence.Repositories;
 
 namespace PlantShop.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly GenericRepository<Product> _productRepository;
+        private readonly GenericRepository<ProductCategory> _productCategoryRepository;
+        private readonly GenericRepository<ProductDiscount> _productDiscountRepository;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public List<Product> Products { get; set; }
+        public List<ProductCategory> ProductCategories { get; set; }
+        public List<ProductDiscount> ProductDiscounts { get; set; }
+
+
+        public IndexModel(ILogger<IndexModel> logger, GenericRepository<Product> productRepository, GenericRepository<ProductCategory> productCategoryRepository, GenericRepository<ProductDiscount> productDiscountRepository)
         {
             _logger = logger;
+            _productRepository = productRepository;
+            _productCategoryRepository = productCategoryRepository;
+            _productDiscountRepository = productDiscountRepository;
+
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-
+            Products = await _productRepository.ListAsync();
+            ProductCategories = await _productCategoryRepository.ListAsync();
+            ProductDiscounts = await _productDiscountRepository.ListAsync();
         }
     }
 }
--- LandingPage/Pages/Products/Index.cshtml.cs
+++ LandingPage/Pages/Products/Index.cshtml.cs
@@ -11,14 +11,18 @@ namespace PlantShop.Pages.Products
     public class IndexModel : PageModel
     {
         private readonly GenericRepository<Product> _productRepository;
+        private readonly GenericRepository<ProductCategory> _productCategoryRepository;
         public List<Product> Products { get; set; }
-        public IndexModel(GenericRepository<Product> productRepository)
+        public List<ProductCategory> ProductCategories { get; set; }
+        public IndexModel(GenericRepository<Product> productRepository, GenericRepository<ProductCategory> productCategoryRepository)
         {
             _productRepository = productRepository;
+            _productCategoryRepository = productCategoryRepository;
         }
         public async Task OnGetAsync()
         {
             Products = await _productRepository.ListAsync();
+            ProductCategories = await _productCategoryRepository.ListAsync();
         }
     }
 }
