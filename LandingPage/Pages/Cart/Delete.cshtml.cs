27b61d2fcc49f213136310103f5b5ff263ca9599
lcht2701 <lecanhhoangthanh@gmail.com>
2023-03-08 00:37:42 +0700

    minor changes

--- LandingPage/Pages/Cart/Delete.cshtml.cs
+++ LandingPage/Pages/Cart/Delete.cshtml.cs
@@ -6,27 +6,27 @@ using System.Threading.Tasks;
 
 namespace PlantShop.Pages.Cart
 {
-    [BindProperties]
-    public class DeleteModel : PageModel
-    {
-        private readonly GenericRepository<CartItem> _cartItemRepository;
-        public CartItem CartItem { get; set; }
-        public DeleteModel(GenericRepository<CartItem> cartItemRepository)
-        {
-            _cartItemRepository = cartItemRepository;
-        }
-        public async Task OnGetAsync(int id)
-        {
-            CartItem = await _cartItemRepository.FindByIdAsync(id);
-        }
-        public async Task<IActionResult> OnPost()
-        {
-            var item = await _cartItemRepository.FindByIdAsync(CartItem.Id);
-            if (item != null)
-            {
-                await _cartItemRepository.SoftDeleteAsync(item);
-            }
-            return Page();
-        }
-    }
+	[BindProperties]
+	public class DeleteModel : PageModel
+	{
+		private readonly GenericRepository<CartItem> _cartItemRepository;
+		public CartItem CartItem { get; set; }
+		public DeleteModel(GenericRepository<CartItem> cartItemRepository)
+		{
+			_cartItemRepository = cartItemRepository;
+		}
+		public async Task OnGetAsync(int id)
+		{
+			CartItem = await _cartItemRepository.FindByIdAsync(id);
+		}
+		public async Task<IActionResult> OnPost()
+		{
+			var item = await _cartItemRepository.FindByIdAsync(CartItem.Id);
+			if (item != null)
+			{
+				await _cartItemRepository.SoftDeleteAsync(item);
+			}
+			return Page();
+		}
+	}
 }
