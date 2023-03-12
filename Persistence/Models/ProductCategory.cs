6461999869274314f3c6cfa0aabd1676bbe3d6fb
hado9 <hado@intelisysaviation.com>
2023-03-12 20:14:09 +0700

    Add category to page index

--- Persistence/Models/ProductCategory.cs
+++ Persistence/Models/ProductCategory.cs
@@ -15,7 +15,7 @@ namespace Persistence.Models
             Products = new HashSet<Product>();
         }
         [Required]
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public string Description { get; set; }
         public virtual ICollection<Product> Products { get; set; }
     }
